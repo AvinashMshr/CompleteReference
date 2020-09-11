d03815fdf63c24c9e9e7cade90fce4cb479afd0f
Avinash Kumar <p.avinashk@gmail.com>
2020-09-11 20:09:02 +0530

    Part1Chap3 second commit

--- Part1Chap3/src/complete/reference/part/one/chap/three/Area.java
+++ Part1Chap3/src/complete/reference/part/one/chap/three/Area.java
@@ -1,2 +1,13 @@
-package complete.reference.part.one.chap.three;public class Area {
+package complete.reference.part.one.chap.three;
+
+public class Area {
+  public static void main(String... arg){
+    double pi, r, a;
+
+    r = 10.8; // radius of circle
+    pi = 3.1416; // pi, approximately
+
+    a = pi * r * r; // compute area
+    System.out.println("Area of circle is " + a);
+  }
 }
--- Part1Chap3/src/complete/reference/part/one/chap/three/ScopeError.java
+++ Part1Chap3/src/complete/reference/part/one/chap/three/ScopeError.java
@@ -1,2 +1,11 @@
-package complete.reference.part.one.chap.three;public class ScopeError {
+package complete.reference.part.one.chap.three;
+
+public class ScopeError {
+  public static void main(String args[]) {
+    int bar = 1;
+    {
+      // creates a new scope
+      //int bar = 2; // Compile-time error – bar already defined!
+    }
+  }
 }
