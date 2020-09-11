ac503d7d10f9d79e62017a070e6ffa4933634070
Avinash Kumar <AvinashMshr@users.noreply.github.com>
2020-09-11 21:53:21 +0530

    Part1 chap3 (#1)

---
+++ Part1Chap3/src/complete/reference/part/one/chap/three/CharDemo.java
@@ -0,0 +1,13 @@
+package complete.reference.part.one.chap.three;
+
+public class CharDemo {
+  public static void main(String... arg){
+    char char1, char2;
+
+    char1 = 88;
+    char2 = 'Y';
+
+    System.out.println("ch1 and ch2: ");
+    System.out.println(char1 + " " + char2);
+  }
+}
